e8c4fa7ff056dee4ed463e53e62a9e044d24da6a
VictorBren0 <victor.bsr2000@gmail.com>
2023-04-10 20:25:45 -0300

    Feat: Componente criado

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,17 @@
+import './card.css';
+
+
+function Card() {
+    return (
+        <div className="container-card">
+            <div className="content-card">
+
+            </div>
+            <div className="content-card2">
+                    
+                </div>
+        </div>
+
+    )
+}
+export default Card;
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,12 @@
+import './input.css';
+
+
+function Input() {
+    return (
+        <div>
+            <h1>Ola mundo</h1>
+        </div>
+
+    )
+}
+export default Input;
